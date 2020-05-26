48d335e2752e77e9f2e525ac426deff80847924a
valivaly1 <64637801+valivaly1@users.noreply.github.com>
2020-05-26 16:48:38 +0300

    Create eui.cpp

---
+++ eui.cpp
@@ -0,0 +1,98 @@
+/*
+ *  LoRaWAN module
+ *
+ *  Copyright (C) Libelium Comunicaciones Distribuidas S.L.
+ *  http://www.libelium.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ *  Version:           0.4
+ *  Design:            David Gascón
+ *  Implementation:    Yuri Carmona & Ruben Martin
+ */
+
+#include "arduPiLoRaWAN.h"
+#include <sw/redis++/redis++.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace sw::redis;
+
+uint8_t sock = SOCKET0;
+uint8_t error;
+/*********************************************************
+ *  IF YOUR ARDUINO CODE HAS OTHER FUNCTIONS APART FROM  *
+ *  setup() AND loop() YOU MUST DECLARE THEM HERE        *
+ * *******************************************************/
+
+/**************************
+ * YOUR ARDUINO CODE HERE *
+ * ************************/
+
+void setup()
+{
+	 error = LoRaWAN.ON(sock);
+
+  // Check status
+  if( error == 0 ) 
+  {
+    printf("1. Switch ON OK\n");
+  }
+  else 
+  {
+    printf("1. Switch ON error = %d\n", error);
+  }
+  
+  error = LoRaWAN.factoryReset();
+
+  // Check status
+  if( error == 0 ) 
+  {
+    printf("2. Reset to factory default values OK\n");
+  }
+  else 
+  {
+    printf("2. Reset to factory error = %d\n", error);
+  }
+
+	
+	error = LoRaWAN.getDeviceEUI();
+
+  // Check status
+  if( error == 0 ) 
+  {
+    printf("3.2. Get Device EUI OK. ");
+    printf("Device EUI: %s\n",LoRaWAN._devEUI);
+  }
+  else 
+  {
+    printf("3.2. Get Device EUI error = %d\n", error);
+  }
+	
+}
+
+void loop(void)
+{
+}
+
+int main (){
+	setup();
+	while(1){
+		loop();
+	}
+	return (0);
+}
+
+//////////////////////////////////////////////
