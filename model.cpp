0cb87f774128ff39de8d35b0e80022484a10a178
valivaly1 <64637801+valivaly1@users.noreply.github.com>
2020-06-04 12:14:02 +0300

    Create model.cpp

---
+++ model.cpp
@@ -0,0 +1,26 @@
+#include "librarie"
+
+
+
+void setup()
+{
+	 // Aici se realizeza setarea initiala a modulului LoRaWAN
+	
+}
+
+void loop(void)
+{
+//Aici se scrie codul care trebuie executat intr-un loop, de exmplu trimiterea de date catre server sau citirea acestora din cache
+}
+
+void functii(){
+// de asemenea se pot realiza si alte functii custom acestea fiind apelate in int main sau in orice alta fuctie care este apelata in int main
+}
+
+int main (){
+	setup();
+	while(1){
+		loop();
+	}
+	return (0);
+}
