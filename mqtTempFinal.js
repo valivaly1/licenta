a1b5199cad2111235a63738b8e63c856b46f315d
valivaly1 <64637801+valivaly1@users.noreply.github.com>
2020-09-13 15:34:18 +0300

    Update mqtTempFinal.js

--- mqtTempFinal.js
+++ mqtTempFinal.js
@@ -14,6 +14,20 @@ client.on('message',(topic,message)=>{
     string = message.toString()
     console.log(message.toString())
     console.log(string)
-    var t1 = string.substring(string.lastIndexOf('T_1":"')+6,string.lastIndexOf$
+    var t1 = string.substring(string.lastIndexOf('T_1":"')+6,string.lastIndexOf('T_1":"')+10)
     console.log(t1)
     var tt1 = Number(t1)*10
+    console.log(tt1)
+    ht1=tt1.toString(16)
+    if(ht1.length < 4){
+            var tmp = 4 - ht1.length;
+            var stmp = dacanue4.substring(0,tmp);
+            ht1f = stmp.concat(ht1).toUpperCase();
+        }
+        else{ht1f=ht1.toUpperCase()}
+    console.log(ht1f)
+    fs.writeFile('temp.txt',ht1f,function (err){
+     if(err) return console.log(err);
+     console.log('a scris');
+})
+})
